8e0508a2afaa2e902e15d53fc34998cc164a90a8
Демиденко Иван Владимирович <ivdemidenk@mts.ru>
2022-08-22 16:04:26 +0300

    Added an example to the topic Transformation and Reduction

---
+++ Transformation_Reduction/Transformation_Reduction/Animal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transformation_Reduction
+{
+    public class Animal
+    {
+       public string Species { get; set; }
+    }
+}
---
+++ Transformation_Reduction/Transformation_Reduction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Transformation_Reduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1) Value types
+            int temperatureVal1 = 30;
+            float temperatureVal2 = 99.53f;
+
+            //temperatureVal2 = temperatureVal1;
+
+            temperatureVal1 = (int)temperatureVal2;
+
+
+            //2) Ref Types
+
+            var animal = new Animal { Species = "marine" };
+            var giraffe = new Giraffe {Species = "land", NeckLength = 10};
+
+            animal = giraffe;
+            var spec = animal.Species;
+
+            giraffe = (Giraffe)animal;
+
+            //Animal animal1 = new Animal();
+            //giraffe = (Giraffe)animal1;
+        }
+    }
+}
