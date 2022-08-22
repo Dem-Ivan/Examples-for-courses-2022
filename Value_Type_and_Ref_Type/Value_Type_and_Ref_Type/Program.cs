0b578a99769eb16b722c4b06c2771484cf4fbb47
Демиденко Иван Владимирович <ivdemidenk@mts.ru>
2022-08-22 14:10:58 +0300

    Добавлен пустой проект

---
+++ Value_Type_and_Ref_Type/Value_Type_and_Ref_Type/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Value_Type_and_Ref_Type
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
