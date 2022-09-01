74dcd520b8838d5d5afb097d8eac3824f8b002b4
Демиденко Иван Владимирович <ivdemidenk@mts.ru>
2022-09-01 15:01:06 +0300

    A short example for the topic Equivalence And Tests

---
+++ EquivalenceAndTests/EquivalenceAndTests/EquivalenceTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EquivalenceAndTests
+{
+   public class EquivalenceTests
+    {
+
+        [Fact]
+        public void TwoObjectsEquivalenceTest()
+        {
+            //Arrange
+            var olga = new Player { Name = "Olga" };
+            var olga2 = new Player { Name = "Olga" };
+
+            //Act
+            var actualResultA = olga == olga2;
+            var actualResultB = olga.Equals(olga2);
+
+            //Assert
+            //Assert.True(actualResultA);
+            Assert.True(actualResultB);  
+        }
+
+        [Fact]
+        public void EqualsNecessityPositivTest()
+        {
+            //Arrange
+            var clientsList = new List<Player>();
+
+            var olga = new Player { Name = "Olga", Number = 321 };
+            var olgaIvanna = new Player { Name = "Olga", Number = 321 };
+
+            clientsList.Add(olga);
+
+
+            //Act
+            var firatOlga = clientsList.FirstOrDefault(p => p.Name == "Olga");
+            var isOlgaInList = clientsList.Contains(olga);
+            var isolgaIvannaInList = clientsList.Contains(olgaIvanna);
+
+            //Assert
+            Assert.True(isolgaIvannaInList);
+        }
+
+        [Fact]
+        public void GetHashCodeNecessityPositivTest()
+        {
+            //Arrange
+            var accointsDyctionary = new Dictionary<Player, Team>();
+
+            var olga = new Player { Name = "Olga" };
+            var olgaIvanna = new Player { Name = "Olga" };
+
+            accointsDyctionary.Add(olga, new Team { Name = "Zinit" });
+
+            //Act
+            var expected = accointsDyctionary[olga];
+            var actual = accointsDyctionary[olgaIvanna];
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ EquivalenceAndTests/EquivalenceAndTests/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquivalenceAndTests
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+
+/*
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (!(obj is Player))
+                return false;
+
+            var player = (Player)obj;
+
+            return player.Name == Name &&
+                   player.Number == Number;
+        }
+
+        public override int GetHashCode()
+        {
+            var resultHash = Name.GetHashCode() + Number.GetHashCode();
+
+            return resultHash;
+        }
+*/
+    }
+}
