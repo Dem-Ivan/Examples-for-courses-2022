b3b32643968fcff35315dfe6cfc4a596c2bbf40e
Демиденко Иван Владимирович <ivdemidenk@mts.ru>
2022-09-05 11:25:56 +0300

    A short example for the topic Exception Handling

---
+++ ExceptionHandling/ExceptionHandling/Storage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{  
+    public class Storage
+    {
+        public readonly List<Player> players = new List<Player>();
+
+        public void AddClient(Player player)
+        {
+            //if (player.Name == null)//3
+            //{
+            //    throw new ArgumentNullException("Имя игрока обязательно для заполнения");
+            //}
+
+            if (player.Number == 0)
+            {
+                throw new NumberRestrictionException("Номер игрока обязательен для заполнения");
+            }
+
+            players.Add(player);
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/StorageTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExceptionHandling
+{
+    public class StorageTests
+    {
+        [Fact]
+        public void AddPlayerWithoutNumber_throw_NumberRestrictionException()
+        {
+            var playerStorage = new Storage();
+            var olga = new Player 
+            {
+                //Name = "Olga",//3
+                Number = 333
+            };
+
+            playerStorage.AddClient(olga);//1
+
+            //try//2
+            //{
+            //   playerStorage.AddClient(olga);
+            //}
+            //catch (NumberRestrictionException e)
+            //{
+            //    Assert.Equal("Номер игрока обязательен для заполнения", e.Message);
+            //    Assert.Equal(typeof(NumberRestrictionException), e.GetType());
+            //}
+            //catch (Exception e)
+            //{
+            //    Assert.True(false);
+            //}
+        }
+
+        [Fact]
+        public void AddPlayerWithoutName_throw_ArgumentNullException()
+        {
+            var playerStorage = new Storage();
+            var olga = new Player
+            {
+                         
+            };
+
+            Assert.Throws<ArgumentNullException>(() => playerStorage.AddClient(olga));//4
+            
+        }
+
+        [Fact]
+        public void AddPlayerPositivTest()
+        {
+
+        }
+    }
+}
