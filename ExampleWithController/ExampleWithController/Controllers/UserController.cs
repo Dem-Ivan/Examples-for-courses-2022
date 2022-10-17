0503bb0d9594891959d4bcbd624aaa7d015a9c27
Демиденко Иван Владимирович <ivdemidenk@mts.ru>
2022-10-17 09:29:25 +0300

    Example With Controller

---
+++ ExampleWithController/ExampleWithController/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleWithController.Controllers
+{
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+        private List<String> _usersNames = new List<string> { "Nycolas", "Semen"};
+
+        [HttpGet]
+        public List<string> GetUserNames()
+        {
+            return _usersNames;
+        }
+
+        [HttpPost]
+        public void AddUserName(string name)
+        {
+            _usersNames.Add(name); 
+        }
+
+        [HttpDelete]
+        public void DeleteUser(string name)
+        {
+            _usersNames.Remove(name);
+        }
+    }
+}
